e9bb70d90fb68e025179ef00ab5322f2d9e3d983
Alvaro Gonzalez Jimenez <agonzj02@estudiantes.unileon.es>
2020-10-22 13:50:04 +0200

    Cambiada forma mostrar peliculas iniciales

--- frontend/src/components/Pelicula.vue
+++ frontend/src/components/Pelicula.vue
@@ -1,45 +1,50 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
-    <v-img :src="picture" height="275px"></v-img>
+  <v-card class="mx-auto" :max-width="width">
+    <v-img :src="picture" :width="width" :height="height"></v-img>
 
     <v-card-title>
       {{ title }}
     </v-card-title>
+    <div v-if="inicial">
+      <v-card-subtitle> {{ generos }} </v-card-subtitle>
 
-    <v-card-subtitle> {{ generos }} </v-card-subtitle>
+      <v-card-actions>
+        <v-btn color="orange lighten-2" text @click="show = !show">
+          Explorar
+        </v-btn>
 
-    <v-card-actions>
-      <v-btn color="orange lighten-2" text @click="show = !show">
-        Explorar
-      </v-btn>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+        </v-btn>
+      </v-card-actions>
 
-      <v-btn icon @click="show = !show">
-        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text >
-          <div>Director : {{ director }}</div>
-          <div class="mt-2">País : {{ country }}</div>
-          <div class="mt-2">Año : {{ year }}</div>
-          <div class="mt-2">
-            <v-btn class="ma-2" outlined color="indigo" :href=link target="_blank">
-              Link IMDB
-            </v-btn>
-          </div>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-    <v-divider dark></v-divider>
+          <v-card-text>
+            <div>Director : {{ director }}</div>
+            <div class="mt-2">País : {{ country }}</div>
+            <div class="mt-2">Año : {{ year }}</div>
+            <div class="mt-2">
+              <v-btn
+                class="ma-2"
+                outlined
+                color="indigo"
+                :href="link"
+                target="_blank"
+              >
+                Link IMDB
+              </v-btn>
+            </div>
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+      <v-divider dark></v-divider>
+    </div>
     <v-card-actions class="pa-4">
-      Puntúa la película
-      <v-spacer></v-spacer>
       <span class="grey--text text--lighten-2 caption mr-2">
         ({{ rating }})
       </span>
@@ -59,7 +64,20 @@
 <script>
 export default {
   name: "Pelicula",
-  props: ['title','imdbID','picture','country','director','actors','genre','id','year'],
+  props: [
+    "title",
+    "imdbID",
+    "picture",
+    "country",
+    "director",
+    "actors",
+    "genre",
+    "id",
+    "year",
+    "inicial",
+    "width",
+    "height"
+  ],
   data: () => ({
     clicked: false,
     show: false,
@@ -68,19 +86,19 @@ export default {
   watch: {
     // whenever question changes, this function will run
     rating: function () {
-      if(!this.clicked){
-        this.clicked = true
-        this.$emit('addReview')
+      if (!this.clicked) {
+        this.clicked = true;
+        this.$emit("addReview");
       }
-    }
+    },
   },
   methods: {
     addReview() {
-      if(!this.clicked){
-        this.clicked = true
-        this.$emit('addReview')
+      if (!this.clicked) {
+        this.clicked = true;
+        this.$emit("addReview");
       }
-   }
+    },
   },
   computed: {
     generos() {
--- frontend/src/views/Inicial.vue
+++ frontend/src/views/Inicial.vue
@@ -1,105 +1,267 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="3" v-for="item in peliculas" :key="item.id">
-                <Pelicula :title="item.title" :id="item.id" :imdbID="item.imdbID" :picture="item.picture" :year="item.year"
-                :country="item.country" :director="item.director" :actors="item.actors" :genre="item.genre" @addReview="reviewed++"></Pelicula>
-            </v-col>
-        </v-row>
-        <div v-if="cambiar">
-            <Alerta/>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div>
+          <p class="font-weight-black text-center text-h4">
+            Prueba, valora al menos 3 películas que te gustan.
+          </p>
         </div>
-    </v-container>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2" v-for="item in peliculas" :key="item.id">
+        <Pelicula
+          :title="item.title"
+          :id="item.id"
+          :imdbID="item.imdbID"
+          :picture="item.picture"
+          :year="item.year"
+          :country="item.country"
+          :director="item.director"
+          :actors="item.actors"
+          :genre="item.genre"
+          :inicial="false"
+          :width="180"
+          :height="268"
+          @addReview="reviewed++"
+        ></Pelicula>
+      </v-col>
+    </v-row>
+    <div v-if="cambiar">
+      <Alerta />
+    </div>
+  </v-container>
 </template>
 
 <script>
 import Pelicula from "@/components/Pelicula.vue";
 import Alerta from "@/components/Alerta.vue";
 
-export default{
-    name: "Inicial",
-    components: {
-        Pelicula,
-        Alerta
+export default {
+  name: "Inicial",
+  components: {
+    Pelicula,
+    Alerta,
+  },
+  watch: {
+    reviewed: function () {
+      if (this.reviewed == 5) {
+        this.cambiar = true;
+      }
     },
-    watch: {
-        reviewed: function () {
-            if(this.reviewed == 5){
-                this.cambiar = true
-            }
-        }
-    },
-    data: () => ({
-        peliculas : [{
-            id : 11,
-            title : 'Jumanji',
-            imdbID: "0113497",
-            picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year:2010
-        },
-        {
-            id : 804,
-            title : 'Ella es unica',
-            imdbID: "0117628",
-            picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year: 2000
-        },
-        {
-            id : 3239,
-            title : 'Ella es unica',
-            imdbID: "0141399",
-            picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year: 1980
-        },
-        {
-            id : 11,
-            title : 'Jumanji',
-            imdbID: "0113497",
-            picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year:2010
-        },
-        {
-            id : 804,
-            title : 'Ella es unica',
-            imdbID: "0117628",
-            picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year: 2000
-        },
-        {
-            id : 3239,
-            title : 'Ella es unica',
-            imdbID: "0141399",
-            picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
-            country: "USA",
-            director: "Joe Johnston",
-            actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
-            genre: ["Adventure", "Fantasy", "Children"],
-            year: 1980
-        }
-        ],
-        reviewed: 0,
+  },
+  data: () => ({
+    peliculas: [
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+      {
+        id: 11,
+        title: "Jumanji",
+        imdbID: "0113497",
+        picture: "http://content8.flixster.com/movie/56/79/73/5679734_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2010,
+      },
+      {
+        id: 804,
+        title: "Ella es unica",
+        imdbID: "0117628",
+        picture: "http://content9.flixster.com/movie/27/16/271699_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 2000,
+      },
+      {
+        id: 3239,
+        title: "Ella es unica",
+        imdbID: "0141399",
+        picture: "http://content7.flixster.com/movie/28/13/281397_det.jpg",
+        country: "USA",
+        director: "Joe Johnston",
+        actors: ["Robin Williams", "James Handy", "Bebe Neuwirth"],
+        genre: ["Adventure", "Fantasy", "Children"],
+        year: 1980,
+      },
+    ],
+    reviewed: 0,
 
-        cambiar: false
-    })
-    }
+    cambiar: false,
+  }),
+};
 </script>
+
+<style lang="css" scoped>
+.p {
+  font-size: 0.8em;
+  font-weight: 100;
+}
+</style>
